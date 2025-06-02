9ec8ae8f805ac645a6d82ead7306acdd4c7d5298
Maxence Poutord <maxence.poutord@gmail.com>
2025-06-02 09:30:38 +0200

    feat: update May 2025

--- vscode/install_plugin.sh
+++ vscode/install_plugin.sh
@@ -1,19 +1,16 @@
+code --install-extension akamud.vscode-theme-onedark
 code --install-extension alefragnani.project-manager
-code --install-extension amazonwebservices.codewhisperer-for-command-line-companion
 code --install-extension astro-build.astro-vscode
 code --install-extension biomejs.biome
 code --install-extension bradlc.vscode-tailwindcss
 code --install-extension christian-kohler.npm-intellisense
 code --install-extension christian-kohler.path-intellisense
 code --install-extension circleci.circleci
-# code --install-extension codeium.codeium
 code --install-extension cssho.vscode-svgviewer
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension donjayamanne.githistory
 code --install-extension eamodio.gitlens
 code --install-extension editorconfig.editorconfig
-code --install-extension equinusocio.vsc-material-theme
-code --install-extension equinusocio.vsc-material-theme-icons
 code --install-extension esbenp.prettier-vscode
 code --install-extension github.copilot
 code --install-extension github.copilot-chat
@@ -23,17 +20,14 @@ code --install-extension graphql.vscode-graphql-execution
 code --install-extension graphql.vscode-graphql-syntax
 code --install-extension hoovercj.vscode-power-mode
 code --install-extension irongeek.vscode-env
-code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension ms-azuretools.vscode-docker
-# code --install-extension ms-python.debugpy
-# code --install-extension ms-python.python
-# code --install-extension ms-python.vscode-pylance
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension octref.vetur
 code --install-extension orta.vscode-jest
 code --install-extension pkief.material-icon-theme
 code --install-extension pnp.polacode
 code --install-extension stylelint.vscode-stylelint
+code --install-extension tamasfe.even-better-toml
 code --install-extension unifiedjs.vscode-mdx
 code --install-extension visualstudioexptteam.intellicode-api-usage-examples
 code --install-extension visualstudioexptteam.vscodeintellicode
@@ -41,7 +35,6 @@ code --install-extension vscode-icons-team.vscode-icons
 code --install-extension vunguyentuan.vscode-postcss
 code --install-extension waderyan.gitblame
 code --install-extension wallabyjs.quokka-vscode
-code --install-extension whizkydee.material-palenight-theme
 code --install-extension wix.vscode-import-cost
 code --install-extension yoavbls.pretty-ts-errors
 code --install-extension znck.grammarly
